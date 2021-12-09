e55a650ab9fa587e7dddc0effefec838d249d52f
more-ginger <francesca1morini@gmail.com>
2021-12-09 11:25:37 +0100

    change names to folders

--- dmacp-app/src/main.js
+++ dmacp-app/src/main.js
@@ -1,10 +1,10 @@
-import App from './App.svelte';
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-const app = new App({
-	target: document.body,
-	// props: {
-	// 	name: 'dmacp'
-	// }
-});
+Vue.config.productionTip = false
 
-export default app;
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
